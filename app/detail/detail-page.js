3eca7db4880c3a821b52e09c741799a9fe03b106
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-02-15 23:58:24 +0100

    change color

--- app/detail/detail-page.js
+++ app/detail/detail-page.js
@@ -9,10 +9,33 @@ var temp;
 var pageData;
 var place;
 
+
+var tempColors = [
+    "#2400d8",
+    "#181cf7",
+    "#2857ff",
+    "#3d87ff",
+    "#56b0ff",
+    "#75d3ff",
+    "#99eaff",
+    "#bcf8ff",
+    "#eaffff",
+    "#ffffea",
+    "#fff1bc",
+    "#ffd699",
+    "#ffff75",
+    "#ff7856",
+    "#ff3d3d",
+    "#f72735",
+    "#d8152f",
+    "#a50021"
+];
+
 function pageLoaded(args) {
     var page = args.object;
     temp = new ObservableArray();
     press = new ObservableArray();
+
 
     pageData = new Observable.fromObject({
         temp: temp,
@@ -35,6 +58,10 @@ function pageLoaded(args) {
                 let day = date.substring(6, 8);
                 let sDateTime=year + "-" + month + "-" + day;
 
+                let color = temp2color(timeSeries[i].t2c);
+                console.log(color);
+                pageData.set("color_temp", color);
+
                 temp.push({key: sDateTime, val: timeSeries[i].t2c});
                 press.push({key: sDateTime, val: timeSeries[i].slp});
             }
@@ -44,3 +71,49 @@ function pageLoaded(args) {
     page.bindingContext = pageData;
 }
 exports.pageLoaded = pageLoaded;
+
+
+function temp2color(temp) {
+    var index=0;
+
+    // -40 -30 -20 -15 -10 -5 0 3 6 9 12 15 18 21 25 30 40 50
+    if (temp>=-40 && temp<-30) {
+        index=0;
+    } else if (temp>=-30 && temp<-20) {
+        index=1;
+    } else if (temp>=-20 && temp<-15) {
+        index=2;
+    } else if (temp>=-15 && temp<-10) {
+        index=3;
+    } else if (temp>=-10 && temp<-5) {
+        index=4;
+    } else if (temp>=-5 && temp<0) {
+        index=5;
+    } else if (temp>=0 && temp<3) {
+        index=6;
+    } else if (temp>=3 && temp<6) {
+        index=7;
+    } else if (temp>=6 && temp<9) {
+        index=8;
+    } else if (temp>=9 && temp<12) {
+        index=9;
+    } else if (temp>=12 && temp<15) {
+        index=10;
+    } else if (temp>=15 && temp<18) {
+        index=11;
+    } else if (temp>=18 && temp<21) {
+        index=12;
+    } else if (temp>=21 && temp<25) {
+        index=13;
+    } else if (temp>=25 && temp<30) {
+        index=14;
+    } else if (temp>=30 && temp<40) {
+        index=15;
+    } else if (temp>=40 && temp<50) {
+        index=16;
+    } else if (temp>=50 ) {
+        index=17;
+    }
+
+    return tempColors[index];
+}
