6eb687617e426a13d15b895a5dea912a7dcdb71b
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-02-15 18:33:58 +0100

    detail page

--- app/detail/detail-page.js
+++ app/detail/detail-page.js
@@ -1,21 +1,41 @@
-const ObservableArray = require("data/observable-array").ObservableArray;
-const Observable = require("data/observable");
 var frameModule = require("tns-core-modules/ui/frame");
-const PageDatilViewModel = require("./detail-view-model");
-const detailViewModel = new PageDatilViewModel;
+var DetailViewModel = require("./detail-view-model");
+var Observable = require("data/observable");
+var ObservableArray = require("data/observable-array").ObservableArray;
+var detailViewModel = new DetailViewModel();
 
-let place;
+var press;
+var temp;
+var pageData;
 
-function pageLoaded(args)
-{
-    const page = args.object;
-    const detail = new Observable.fromObject({});
+function pageLoaded(args) {
+    var page = args.object;
+    temp = new ObservableArray();
+    press = new ObservableArray();
 
-    place = page.navigationContext.place;
-    console.log(place);
+    pageData = new Observable.fromObject({
+        temp: temp,
+        press: press
+    });
 
-    page.bindingContext = detail;
+
+    fetch("https://api.meteo.uniparthenope.it/products/wrf5/timeseries/com63049?step=24")
+        .then((response) => response.json())
+        .then((data) => {
+            var timeSeries=data['timeseries'];
+            for (var i = 0; i < timeSeries.length; i++) {
+                let date=timeSeries[i].dateTime;
+                let year = date.substring(0, 4);
+                let month = date.substring(4, 6);
+                let day = date.substring(6, 8);
+                let sDateTime=year + "-" + month + "-" + day;
+
+                temp.push({key: sDateTime, val: timeSeries[i].t2c});
+                press.push({key: sDateTime, val: timeSeries[i].slp});
+            }
+        })
+        .catch(error => console.error("ERROR DATA ", error));
+
+    page.bindingContext = pageData;
 }
-
 exports.pageLoaded = pageLoaded;
-
