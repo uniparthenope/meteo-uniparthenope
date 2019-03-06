06a02a1fb9e6924d356364d711323c48584598a3
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-03-06 13:23:33 +0100

    add selection of step hours for graphic in detail page

--- app/detail/detail-page.js
+++ app/detail/detail-page.js
@@ -9,7 +9,6 @@ var getViewById = require("tns-core-modules/ui/core/view").getViewById;
 const appSetting = require("application-settings");
 var dialog = require("tns-core-modules/ui/dialogs");
 var imageSource = require("image-source");
-var ExpandableListItem = require("na");
 
 var press;
 var temp;
@@ -23,8 +22,11 @@ var products;
 var outputs;
 var prod = "wrf5";
 var output = "gen";
+var step = "6";
+var hour = "0";
 let products_map = new Map();
 let outputs_map = new Map();
+var hours;
 
 let mesi = ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"];
 
@@ -35,6 +37,7 @@ function pageLoaded(args) {
     array = new ObservableArray();
     products = new ObservableArray();
     outputs = new ObservableArray();
+    hours = new ObservableArray();
 
     pageData = new Observable.fromObject({
         temp: temp,
@@ -42,7 +45,8 @@ function pageLoaded(args) {
         statistic: array,
         map:map,
         products: products,
-        outputs: outputs
+        outputs: outputs,
+        hours: hours
     });
     pageData.set("isBusy", true);//Load animation
     pageData.set("isHeigh", "25");
@@ -63,158 +67,19 @@ function pageLoaded(args) {
     console.log("[DATA DETTAGLI]" + data);
     pageData.set("titolo", place);
 
-    fetch("https://api.meteo.uniparthenope.it/products/wrf5/timeseries/"+ id +"?step=1")
-        .then((response) => response.json())
-        .then((data) => {
-            var timeSeries=data['timeseries'];
-            for (var i = 0; i < timeSeries.length; i++) {
-                let date=timeSeries[i].dateTime;
-                let year = date.substring(0, 4);
-                let month = date.substring(4, 6);
-                let day = date.substring(6, 8);
-                let ora = date.substring(9,11);
-                let sDateTime=month + "-" + day + "H" + ora;
+    print_chart(id, prod, output, hour, step);
 
-                temp.push({key: sDateTime, val: timeSeries[i].t2c});
-                press.push({key: sDateTime, val: timeSeries[i].slp});
-            }
-        })
-        .then(function () {
-            pageData.set("isBusy_graphic", false);
-            pageData.set("isHeigh_graphic", "0");
-            pageData.set("graphic", "visible");
-        }).catch(error => console.error("[GRAFICO] ERROR DATA ", error));
+    print_meteo(id, data);
 
-    fetch("https://api.meteo.uniparthenope.it/products/wrf5/forecast/" + id + "?date=" + data)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.result == "ok")
-            {
-                if (appSetting.getNumber("Temperatura", 0) == 0)
-                    pageData.set("temperatura", data.forecast.t2c + " °C");
-                else if (appSetting.getNumber("Temperatura", 0) == 1) {
-                    pageData.set("temperatura", ((data.forecast.t2c * 1.8) + 32).toFixed(2) + " °F");
-                }
+    print_series(id);
 
-                pageData.set("_meteo", data.forecast.text);
-                pageData.set("cloud", (data.forecast.clf * 100).toFixed(2) + " %");
-                pageData.set("humidity", (data.forecast.rh2).toFixed(2) + " %");
+    print_map(id, prod, output, data);
 
-                if(appSetting.getNumber("Pressione", 0) == 0)
-                    pageData.set("pressione", data.forecast.slp + " hPa");
-                else if(appSetting.getNumber("Pressione", 0) == 1)
-                    pageData.set("pressione", data.forecast.slp + " millibar");
-                else if(appSetting.getNumber("Pressione", 0) == 2)
-                    pageData.set("pressione", (data.forecast.slp * 0.75006).toFixed(2) + " mmHg");
+    print_prod();
 
-                pageData.set("wind_direction", data.forecast.wd10 + " N");
+    print_output(prod);
 
-                if (appSetting.getNumber("Vento", 0) == 0)
-                    pageData.set("wind_speed", data.forecast.ws10n + " kn");
-                else if (appSetting.getNumber("Vento", 0) == 1) {
-                    pageData.set("wind_speed", (data.forecast.ws10n * 1.852).toFixed(2) + " km/h");
-                } else if (appSetting.getNumber("Vento", 0) == 2) {
-                    pageData.set("wind_speed", (data.forecast.ws10n * 0.514444).toFixed(2) + " m/s");
-                } else if (appSetting.getNumber("Vento", 0) == 3) {
-                    pageData.set("wind_speed", (get_beaufort(data.forecast.ws10n)) + " beaufort");
-                }
-
-                if (appSetting.getNumber("Temperatura", 0) == 0)
-                    pageData.set("wind_chill", data.forecast.wchill + " °C");
-                else if (appSetting.getNumber("Temperatura", 0) == 1) {
-                    pageData.set("wind_chill", ((data.forecast.wchill * 1.8) + 32).toFixed(2) + " °F");
-                }
-
-                pageData.set("wind", data.forecast.winds);
-            }
-            else if (data.result == "error")
-            {
-                dialog.alert({title: "Errore", message: data.details, okButtonText: "OK"});
-            }
-        })
-        .then(function () {
-        pageData.set("isBusy_meteo", false);
-        pageData.set("isHeigh_meteo", "0");
-        pageData.set("meteo", "visible");
-    })
-        .catch(error => console.error("ERROR DATA ", error));
-
-
-    fetch("https://api.meteo.uniparthenope.it/products/wrf5/timeseries/" + id +"?hours=0&step=24")
-        .then((response) => response.json())
-        .then((data) =>
-        {
-            pageData.set("altezza", data.timeseries.length * 45);
-            for(let i=0; i<data.timeseries.length; i++)
-            {
-                let weekDayLabel=dayOfWeek(data.timeseries[i]['dateTime']) + " - " + data.timeseries[i]['dateTime'].substring(6,8) + " " + monthOfYear(data.timeseries[i]['dateTime']);
-                array.push({"forecast":weekDayLabel, "image": "~/meteo_icon/" + data.timeseries[i].icon, "TMin": data.timeseries[i]['t2c-min'], "TMax": data.timeseries[i]['t2c-max'], "Wind":data.timeseries[i].ws10n + " " + data.timeseries[i].winds, "Rain": data.timeseries[i].crh});
-            }
-        })
-        .then(function () {
-            pageData.set("isBusy", false);
-            pageData.set("isHeigh", "0");
-            pageData.set("table", "visible");
-        }).catch(error => console.error("[LABEL] ERROR DATA", error));
-
-    var url_map = "https://api.meteo.uniparthenope.it/products/" + prod + "/forecast/" + id + "/plot/image?date=" + data + "&output=" + output;
-
-    console.log(url_map);
-
-    imageSource.fromUrl(url_map)
-        .then(function () {
-            pageData.map = url_map;
-        }).then(function () {
-            pageData.set("isBusy_map", false);
-            pageData.set("isHeigh_map", "0");
-            pageData.set("_map", "visible");
-        })
-        .catch(err => {
-            console.log("Somthing went wrong!");
-        });
-
-
-    fetch("https://api.meteo.uniparthenope.it/products").then((response) => response.json()).then((data) =>
-    {
-        let product = data['products'];
-
-        var keys = Object.keys(product);
-        var key;
-        for(var i=0; i<keys.length; i++){
-            var val = keys[i];
-            key = product[val]['desc']['it'];
-            products_map.set(key, val);
-            products.push(key);
-        }
-
-        let _prod = [...products_map.entries()]
-            .filter(({ 1: v }) => v === prod)
-            .map(([k]) => k);
-
-        console.log(_prod);
-        pageData.set("hint_prod", _prod);
-    });
-
-    fetch("https://api.meteo.uniparthenope.it/products/" + prod).then((response) => response.json()).then((data) =>
-    {
-        let product = data['outputs']['outputs'];
-
-        var keys = Object.keys(product);
-        var key;
-        for(var i=0; i<keys.length; i++){
-            var val = keys[i];
-            key = product[val]['it'];
-            outputs_map.set(key, val);
-            outputs.push(key);
-        }
-
-        let _out = [...outputs_map.entries()]
-            .filter(({ 1: v }) => v === output)
-            .map(([k]) => k);
-
-        console.log(_out);
-        pageData.set("hint_output", _out);
-    });
+    print_hours();
 
     pageData.set("products", products);
     pageData.set("outputs", outputs);
@@ -231,60 +96,24 @@ exports.dropDownSelectedIndexChanged = function (args) {
     outputs.splice(0);
 
     pageData.set("outputs", null);
-
-    fetch("https://api.meteo.uniparthenope.it/products/" + prod).then((response) => response.json()).then((data) =>
-    {
-        let product = data['outputs']['outputs'];
-
-        var keys = Object.keys(product);
-        var key;
-        for(var i=0; i<keys.length; i++){
-            var val = keys[i];
-            key = product[val]['it'];
-            outputs_map.set(key, val);
-            outputs.push(key);
-        }
-
-        let _out = [...outputs_map.entries()]
-            .filter(({ 1: v }) => v === output)
-            .map(([k]) => k);
-
-        console.log(_out);
-        pageData.set("hint_output", _out);
-    });
+    print_output(prod);
     pageData.set("outputs", outputs);
-
-    var url_map = "https://api.meteo.uniparthenope.it/products/" + prod + "/forecast/" + id + "/plot/image?date=" + data + "&output=" + output;
-    console.log(url_map);
-
-    imageSource.fromUrl(url_map)
-        .then(function () {
-            pageData.map = url_map;
-        })
-        .then(function () {
-        })
-        .catch(err => {
-            console.log("Somthing went wrong!");
-        });
+    print_map(id, prod, output, data);
 };
 
 exports.dropDownSelectedIndexChanged1 = function (args) {
     var out = outputs.getItem(args.object.selectedIndex);
     output = outputs_map.get(out);
-    console.log(output);
 
-    var url_map = "https://api.meteo.uniparthenope.it/products/" + prod + "/forecast/" + id + "/plot/image?date=" + data + "&output=" + output;
-    console.log(url_map);
+    print_map(id, prod, output, data);
+};
 
-    imageSource.fromUrl(url_map)
-        .then(function () {
-            pageData.map = url_map;
-        })
-        .then(function () {
-        })
-        .catch(err => {
-            console.log("Somthing went wrong!");
-        });
+exports.dropDownSelectedIndexChanged2 = function (args) {
+    var h = hours.getItem(args.object.selectedIndex);
+
+    temp.splice(0);
+    press.splice(0);
+    print_chart(id, prod, output, hours, h);
 };
 
 function onTap(args) {
@@ -355,3 +184,184 @@ function monthOfYear(date) {
     let month = parseInt(date.substring(4, 6)) - 1;
     return isNaN(month) ? null : ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"][month];
 };
+
+function print_chart(place, product, output, hours, step)
+{
+    let url = "https://api.meteo.uniparthenope.it/products/" + product + "/timeseries/" + place + "?step=" + step;
+    console.log(url);
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            var timeSeries=data['timeseries'];
+            for (var i = 0; i < timeSeries.length; i++) {
+                let date=timeSeries[i].dateTime;
+                let month = date.substring(4, 6);
+                let day = date.substring(6, 8);
+                let ora = date.substring(9,11);
+                let sDateTime=month + "-" + day + "H" + ora;
+
+                temp.push({key: sDateTime, val: (timeSeries[i].t2c)});
+                press.push({key: sDateTime, val: (timeSeries[i].slp)});
+            }
+        })
+        .then(function () {
+            pageData.set("isBusy_graphic", false);
+            pageData.set("isHeigh_graphic", "0");
+            pageData.set("graphic", "visible");
+        }).catch(error => console.error("[GRAFICO] ERROR DATA ", error));
+
+    return;
+}
+
+function print_meteo(id, data)
+{
+    fetch("https://api.meteo.uniparthenope.it/products/wrf5/forecast/" + id + "?date=" + data)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.result == "ok")
+            {
+                if (appSetting.getNumber("Temperatura", 0) == 0)
+                    pageData.set("temperatura", data.forecast.t2c + " °C");
+                else if (appSetting.getNumber("Temperatura", 0) == 1) {
+                    pageData.set("temperatura", ((data.forecast.t2c * 1.8) + 32).toFixed(2) + " °F");
+                }
+
+                pageData.set("_meteo", data.forecast.text);
+                pageData.set("cloud", (data.forecast.clf * 100).toFixed(2) + " %");
+                pageData.set("humidity", (data.forecast.rh2).toFixed(2) + " %");
+
+                if(appSetting.getNumber("Pressione", 0) == 0)
+                    pageData.set("pressione", data.forecast.slp + " hPa");
+                else if(appSetting.getNumber("Pressione", 0) == 1)
+                    pageData.set("pressione", data.forecast.slp + " millibar");
+                else if(appSetting.getNumber("Pressione", 0) == 2)
+                    pageData.set("pressione", (data.forecast.slp * 0.75006).toFixed(2) + " mmHg");
+
+                pageData.set("wind_direction", data.forecast.wd10 + " N");
+
+                if (appSetting.getNumber("Vento", 0) == 0)
+                    pageData.set("wind_speed", data.forecast.ws10n + " kn");
+                else if (appSetting.getNumber("Vento", 0) == 1) {
+                    pageData.set("wind_speed", (data.forecast.ws10n * 1.852).toFixed(2) + " km/h");
+                } else if (appSetting.getNumber("Vento", 0) == 2) {
+                    pageData.set("wind_speed", (data.forecast.ws10n * 0.514444).toFixed(2) + " m/s");
+                } else if (appSetting.getNumber("Vento", 0) == 3) {
+                    pageData.set("wind_speed", (get_beaufort(data.forecast.ws10n)) + " beaufort");
+                }
+
+                if (appSetting.getNumber("Temperatura", 0) == 0)
+                    pageData.set("wind_chill", data.forecast.wchill + " °C");
+                else if (appSetting.getNumber("Temperatura", 0) == 1) {
+                    pageData.set("wind_chill", ((data.forecast.wchill * 1.8) + 32).toFixed(2) + " °F");
+                }
+
+                pageData.set("wind", data.forecast.winds);
+            }
+            else if (data.result == "error")
+            {
+                dialog.alert({title: "Errore", message: data.details, okButtonText: "OK"});
+            }
+        })
+        .then(function () {
+            pageData.set("isBusy_meteo", false);
+            pageData.set("isHeigh_meteo", "0");
+            pageData.set("meteo", "visible");
+        }).catch(error => console.error("ERROR DATA ", error));
+
+    return;
+}
+
+function print_series(id)
+{
+    fetch("https://api.meteo.uniparthenope.it/products/wrf5/timeseries/" + id +"?hours=0&step=24")
+        .then((response) => response.json())
+        .then((data) =>
+        {
+            pageData.set("altezza", data.timeseries.length * 45);
+            for(let i=0; i<data.timeseries.length; i++)
+            {
+                let weekDayLabel=dayOfWeek(data.timeseries[i]['dateTime']) + " - " + data.timeseries[i]['dateTime'].substring(6,8) + " " + monthOfYear(data.timeseries[i]['dateTime']);
+                array.push({"forecast":weekDayLabel, "image": "~/meteo_icon/" + data.timeseries[i].icon, "TMin": data.timeseries[i]['t2c-min'], "TMax": data.timeseries[i]['t2c-max'], "Wind":data.timeseries[i].ws10n + " " + data.timeseries[i].winds, "Rain": data.timeseries[i].crh});
+            }
+        })
+        .then(function () {
+            pageData.set("isBusy", false);
+            pageData.set("isHeigh", "0");
+            pageData.set("table", "visible");
+        }).catch(error => console.error("[LABEL] ERROR DATA", error));
+
+    return;
+}
+
+function print_map(id, prod, output, data)
+{
+    var url_map = "https://api.meteo.uniparthenope.it/products/" + prod + "/forecast/" + id + "/plot/image?date=" + data + "&output=" + output;
+
+    imageSource.fromUrl(url_map)
+        .then(function () {
+            pageData.map = url_map;
+        }).then(function () {
+        pageData.set("isBusy_map", false);
+        pageData.set("isHeigh_map", "0");
+        pageData.set("_map", "visible");
+    }).catch(err => {console.log("Somthing went wrong!");});
+}
+
+function print_prod()
+{
+    fetch("https://api.meteo.uniparthenope.it/products").then((response) => response.json()).then((data) =>
+    {
+        let product = data['products'];
+
+        var keys = Object.keys(product);
+        var key;
+        for(var i=0; i<keys.length; i++){
+            var val = keys[i];
+            key = product[val]['desc']['it'];
+            products_map.set(key, val);
+            products.push(key);
+        }
+
+        let _prod = [...products_map.entries()]
+            .filter(({ 1: v }) => v === prod)
+            .map(([k]) => k);
+
+        console.log(_prod);
+        pageData.set("hint_prod", _prod);
+    });
+}
+
+function print_output(prod)
+{
+    fetch("https://api.meteo.uniparthenope.it/products/" + prod).then((response) => response.json()).then((data) =>
+    {
+        let product = data['outputs']['outputs'];
+
+        var keys = Object.keys(product);
+        var key;
+        for(var i=0; i<keys.length; i++){
+            var val = keys[i];
+            key = product[val]['it'];
+            outputs_map.set(key, val);
+            outputs.push(key);
+        }
+
+        let _out = [...outputs_map.entries()]
+            .filter(({ 1: v }) => v === output)
+            .map(([k]) => k);
+
+        console.log(_out);
+        pageData.set("hint_output", _out);
+    });
+}
+
+function print_hours()
+{
+    var h = ['1', '3', '6', '9', '12', '24'];
+    for(let i=0; i<h.length; i++)
+    {
+        hours.push(h[i]);
+    }
+    pageData.set("hint_hours", "6");
+}
