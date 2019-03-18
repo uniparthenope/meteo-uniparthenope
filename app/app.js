e416f538c04823d25043bb2d17f7c0ca2661c5dd
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-03-18 20:59:16 +0100

    add multi-language

--- app/app.js
+++ app/app.js
@@ -1,13 +1,6 @@
 let application = require("tns-core-modules/application");
 var frame = require("ui/frame");
-
-const platformModule = require("tns-core-modules/platform");
-let language = platformModule.device.language;
-if(language != 'it')
-    language == 'en'
-console.log("Lingua: " + language);
-
-require('tns-i18n')(language);
+require('tns-i18n')('en');
 
 global.contatore = 0;
 global.place_selected = "";
