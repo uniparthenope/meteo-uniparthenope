cf2f442ad3683bfcaa6cf81364cabb784c9a3a24
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-03-19 12:24:57 +0100

    multi-language

--- app/disclaimer/disclaimer-page.js
+++ app/disclaimer/disclaimer-page.js
@@ -3,17 +3,19 @@ const Observable = require("data/observable");
 const DisclaimerViewModel = require("./disclaimer-view-model");
 const discViewModel = new DisclaimerViewModel;
 var frameModule = require("tns-core-modules/ui/frame");
+const platformModule = require("tns-core-modules/platform");
 
 function pageLoaded(args)
 {
     const page = args.object;
     const disc = new Observable.fromObject({});
     var url_policy = "https://api.meteo.uniparthenope.it/legal/disclaimer";
+    var lingua = platformModule.device.language;
 
     fetch(url_policy)
         .then((response) => response.json())
         .then((data1) => {
-            disc.set("policy", data1.disclaimer.it);
+            disc.set("policy", data1['disclaimer'][lingua]);
         })
         .catch(error => console.error("[DISC] ERROR DATA ", error));
 
